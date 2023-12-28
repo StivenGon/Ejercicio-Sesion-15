db98ac582df6adfcc8001efb74ece3a174df5e3f
StivenGon <oscarstiven.2005@gmail.com>
2023-12-28 15:16:02 -0300

    Ejercicio

---
+++ Practica10/src/principal/Principal.java
@@ -0,0 +1,19 @@
+/**
+ * @author Stiven Gonzalez
+ * 
+ */
+
+
+package principal;
+
+import ventanas.MenuPrincipal;
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+		MenuPrincipal objetoFrame = new MenuPrincipal();
+		objetoFrame.setVisible(true);;
+	}
+
+}
